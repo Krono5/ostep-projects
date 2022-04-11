26d09faef966264b0a90c525400cb4bf697b5d2c
Tyler Storr <tystorr@gmail.com>
2022-04-10 23:43:20 -0400

    up to 5 passes

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -7,24 +7,18 @@
 
 
 int main(int argc, char *argv[]) {
-    char *buffer[BUFF_SIZE];
-    int numLines = 0;
-    size_t lineBufSize = 0;
     int fileNum = 1;
     FILE *input_file = NULL;
     char concatString[9999];
-    char* tempstring;
 
     if (argc == 1) {
         printf("wzip: file1 [file2 ...]\n");
         return 1;
     }
 
-    for (int i = 0; i < BUFF_SIZE; ++i) {
-        buffer[i] = NULL;
-    }
-
-    while (fileNum < argc) {\
+    while (fileNum < argc) {
+        char* tempstring;
+        size_t lineBufSize;
         int numChars = 1;
         input_file = freopen(argv[fileNum], "r", stdin);
         stdin = input_file;
@@ -37,29 +31,31 @@ int main(int argc, char *argv[]) {
         fileNum++;
     }
 
-//    for (int i = 0; i < numLines; ++i) {
-//        strcat(concatString, buffer[i]);
-//    }
-    char currChar = ' ';
+    compressFile(concatString);
+
+    return 0;
+}
+
+void compressFile(char * inputLine){
+    char currChar;
     int currPos = 0;
     int charCount = 0;
 
-    currChar = concatString[0];
-    while (currPos <= strlen(concatString)) {
-        if (concatString[currPos] == currChar) {
+    currChar = inputLine[0];
+    while (currPos <= strlen(inputLine)) {
+        if (inputLine[currPos] == currChar) {
             charCount++;
         } else {
             writeNum(charCount);
             writeChar(currChar);
-            currChar = concatString[currPos];
+            currChar = inputLine[currPos];
             charCount = 1;
         }
-        if (currPos == strlen(concatString)) {
+        if (currPos == strlen(inputLine)) {
             break;
         }
         currPos++;
     }
-    return 0;
 }
 
 void writeChar(char character) {
--- initial-utilities/wzip/wzip.h
+++ initial-utilities/wzip/wzip.h
@@ -7,4 +7,6 @@ void writeChar(char character);
 
 void writeNum(int charCount);
 
+void compressFile(char * inputLine);
+
 #endif //WZIP_H
