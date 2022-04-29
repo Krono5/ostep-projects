9d98288ec3964462f3157e3b51476ea99698fa10
Tyler Storr <tystorr@gmail.com>
2022-04-29 00:42:53 -0400

    adding threading

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -1,12 +1,18 @@
+#include <string.h>
+#include <stdbool.h>
+#include <sys/mman.h>
 #include <malloc.h>
+#include "stdio.h"
 #include "wzip.h"
-#include "sys/mman.h"
 
 
 int main(int argc, char *argv[]) {
     FILE *input_file = NULL;
+    bool isFirstChar = true;
+    char lastChar;
     char *src;
-    bool firstRead = true;
+    int charCount = 1;
+    char firstChar;
 
     // NO FILES TO PARSE
     if (argc == 1) {
@@ -24,77 +30,45 @@ int main(int argc, char *argv[]) {
 
         // IF FILE SIZE IS GREATER THAN 4K CREATE THREADS
         if (fileStat.st_size > 4096) {
-            parseThreaded(src);
-            if (firstRead) {
+            pthread_t threads[3];
+            arg_t arg[3];
+            char *strings[3];
+            strings[0] = malloc(sizeof(src) / 3 + 1);
+            strings[1] = malloc(sizeof(src) / 3 + 1);
+            strings[2] = malloc(sizeof(src) / 3 + 1);
 
-                firstRead = false;
-            } else {
+            strncpy(strings[0], src, strlen(src) / 3);
+            strncpy(strings[1], &src[strlen(src) / 3], strlen(src) / 3);
+            strncpy(strings[2], &src[2 * (strlen(src) / 3)], strlen(src) / 3 + 1);
 
-            }
+            init_arg(arg[0], strings[0], &isFirstChar);
+            init_arg(arg[1], strings[1], &isFirstChar);
+            init_arg(arg[2], strings[2], &isFirstChar);
+
+            pthread_create(&threads[0], NULL, worker, &arg[0]);
+            pthread_create(&threads[1], NULL, worker, &arg[1]);
+            pthread_create(&threads[2], NULL, worker, &arg[2]);
+
+            pthread_join(threads[0], NULL);
+            pthread_join(threads[1], NULL);
+            pthread_join(threads[2], NULL);
         }
-            // IF FILE IS LESS THAN 4K USE THIS MAIN THREAD
+            // FILE SIZE IS LESS THAN 4K
         else {
-            if (firstRead) {
-
-                firstRead = false;
-            } else {
-
-            }
-
+            parse(src, &isFirstChar, &charCount, &lastChar, &firstChar);
         }
         fclose(input_file);
     }
 
     if (charCount > 0) {
-        writeNum(charCount);
-        writeChar(lastChar);
+        res_pair *pair = malloc(sizeof(res_pair));
+        pair->numCharacters = charCount;
+        pair->character = lastChar;
+        writePair(*pair);
+        free(pair);
     }
 
     return 0;
-}
-
-void writeChar(char character) {
-    fwrite(&character, 1, 1, stdout);
-}
-
-void writeNum(int charCount) {
-    fwrite(&charCount, 4, 1, stdout);
-}
-
-/**
- * Thread worker to get the number of characters in order in the section provided
- * @param arg struct of input and output from thread
- * @return
- */
-void *worker(arg_t arg) {
-    char currChar;
-    char lastChar = '\0';
-    int charCount = 0;
-    int charIndex = 0;
-
-
-    // GET THE FIRST CHAR OF THE FILE
-    currChar = arg.arg_val.inputString[0];
-    arg.ret_val.firstChar = currChar;
-    do {
-        // SAME CHAR INCREMENT OR FIRST CHAR
-        if ((currChar == lastChar) || lastChar == '\0') {
-            charCount++;
-            lastChar = currChar;
-            charIndex++;
-            currChar = arg.arg_val.inputString[charIndex];
-        }
-            // NOT THE SAME CHAR, PRINT CURRENT COUNT AND MOVE ON
-        else if (currChar != lastChar) {
-            writePair(arg, lastChar, charCount);
-            charCount = 1;
-            lastChar = currChar;
-            charIndex++;
-            currChar = arg.arg_val.inputString[charIndex];
-        }
-    } while (charIndex <= strlen(arg.arg_val.inputString));
-    arg.ret_val.lastChar = lastChar;
-    return NULL;
 }
 
 /**
@@ -102,86 +76,58 @@ void *worker(arg_t arg) {
  * @param arg
  * @param inputString
  */
-void init_arg(arg_t arg, char *inputString) {
-    arg.arg_val.inputString = inputString;
-    arg.ret_val.numPairs = 0;
+void init_arg(arg_t arg, char *inputString, bool *isFirstChar) {
+    arg.arg_val.src = inputString;
+    *arg.arg_val.isFirstChar = isFirstChar;
     arg.ret_val.resultPairs = NULL;
-    arg.ret_val.firstChar = '\0';
-    arg.ret_val.lastChar = '\0';
 }
 
-/**
- * Write the pair of character to character count to the arg buffer
- * @param arg
- * @param character
- * @param charCount
- */
-void writePair(arg_t arg, char character, int charCount) {
-    ret_pair newPair;
-    newPair.character = character;
-    newPair.numCharacters = charCount;
-    if (arg.ret_val.resultPairs == NULL) {
-        arg.ret_val.resultPairs = malloc(sizeof(ret_pair));
-    } else {
-        arg.ret_val.resultPairs = realloc(arg.ret_val.resultPairs, sizeof(arg.ret_val.resultPairs) + sizeof(ret_pair));
-    }
-    arg.ret_val.resultPairs[arg.ret_val.numPairs] = newPair;
-    arg.ret_val.numPairs++;
+void *worker(arg_t arg) {
+    parse(arg.arg_val.src, arg.arg_val.isFirstChar, arg.arg_val.charCount, arg.arg_val.lastChar, arg.arg_val.firstChar);
+    return NULL;
 }
 
-/**
- * Input is greater than 4K. Create threads
- * @param src
- */
-void parseThreaded(char *src) {
-    pthread_t threads[3];
-    arg_t arg[3];
-    char *strings[3];
+void parse(char *src, bool *isFirstChar, int *charCount, char *lastChar, char *firstCharacter) {
+    char currChar;
 
-    // INITIALIZE THE STRINGS
-    for (int i = 0; i < 3; ++i) {
-        strings[i] = malloc((fileStat.st_size / 3) + 1);
-        memset(strings[i], '\0', sizeof(strlen(strings[i])));
-    }
+    // GET THE FIRST CHAR OF THE FILE
+    int charPos = 0;
+    currChar = src[charPos];
+    *firstCharacter = currChar;
 
-    strncpy(strings[0], src, fileStat.st_size / 3);
-    strncpy(strings[1], src + fileStat.st_size, fileStat.st_size / 3);
-    strncpy(strings[2], src + 2 * (fileStat.st_size), fileStat.st_size / 3);
-
-    init_arg(arg[0], strings[0]);
-    init_arg(arg[1], strings[1]);
-    init_arg(arg[2], strings[2]);
-
-    pthread_create(&threads[0], NULL, worker, &arg[0]);
-    pthread_create(&threads[1], NULL, worker, &arg[1]);
-    pthread_create(&threads[2], NULL, worker, &arg[2]);
-
-    pthread_join(threads[0], NULL);
-    pthread_join(threads[1], NULL);
-    pthread_join(threads[2], NULL);
-
-    // IF THE LAST CHAR OF THE FIRST THREAD MATCHES THE FIRST OF THE 2ND JOIN
-
-//    if (arg[0].ret_val.resultPairs[arg[0].ret_val.numPairs].character ==
-//        arg[1].ret_val.resultPairs[0].character) {
-//        arg[0].ret_val.resultPairs[arg[0].ret_val.numPairs].numCharacters += arg[1].ret_val.resultPairs[0].numCharacters;
-//        arg[1].ret_val.resultPairs[0].character = '\0';
-//    }
-    // IF THE LAST CHAR OF THE 2ND THREAD MATCHES THE FIRST OF THE 3RD JOIN
-    if (arg[1].ret_val.resultPairs[arg[1].ret_val.numPairs].character ==
-        arg[2].ret_val.resultPairs[0].character) {
-        arg[1].ret_val.resultPairs[arg[1].ret_val.numPairs].numCharacters += arg[2].ret_val.resultPairs[0].numCharacters;
-        arg[2].ret_val.resultPairs[0].character = '\0';
-    }
-
-    int numPairs = arg[0].ret_val.numPairs + arg[1].ret_val.numPairs + arg[2].ret_val.numPairs;
-    buffer = realloc(buffer, sizeof(buffer) + (numPairs * (sizeof (char ) + sizeof (int ))));
-
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < arg[i].ret_val.numPairs; ++j) {
-            if(arg[i].ret_val.resultPairs[j].character != '\0'){
-
-            }
+    do {
+        // IF THIS IS THE FIRST CHAR EVER READ SKIP COMPARISON
+        if (*isFirstChar) {
+            *isFirstChar = false;
+            *lastChar = currChar;
+            charPos++;
+            currChar = src[charPos];
         }
-    }
+            // NOT THE SAME CHAR, PRINT CURRENT COUNT AND MOVE ON
+        else if (currChar != *lastChar) {
+            res_pair *pair = malloc(sizeof(res_pair));
+            pair->character = *lastChar;
+            pair->numCharacters = *charCount;
+            writePair(*pair);
+            free(pair);
+            *charCount = 1;
+            *lastChar = currChar;
+            charPos++;
+            currChar = src[charPos];
+        }
+            // SAME CHAR INCREMENT
+        else {
+            *charCount = *charCount + 1;
+            *lastChar = currChar;
+            charPos++;
+            currChar = src[charPos];
+        }
+    } while (charPos < strlen(src));
+
+    return;
+}
+
+void writePair(res_pair pair) {
+    fwrite(&pair.numCharacters, 4, 1, stdout);
+    fwrite(&pair.character, 1, 1, stdout);
 }
--- initial-utilities/wzip/wzip.h
+++ initial-utilities/wzip/wzip.h
@@ -7,43 +7,41 @@
 #include "stdio.h"
 #include "pthread.h"
 
-typedef struct __ret_pair {
+typedef struct __res_pair {
     char character;
     int numCharacters;
-} ret_pair;
+} res_pair;
 
 typedef struct __arg {
     struct arg_val_t {
-        char *inputString;
+        char *src;
+        bool *isFirstChar;
+        int *charCount;
+        char *lastChar;
+        char *firstChar;
     } arg_val;   // information passed to the thread
 
     struct ret_val_t {
-        int numPairs;
-        char firstChar;
-        char lastChar;
-        ret_pair *resultPairs;
+        char firstCharacter;
+        char lastCharacter;
+        res_pair *resultPairs;
     } ret_val;   // information passed back to the main thread
 } arg_t;
 
-typedef struct __file_res{
-    ret_pair *filePairs;
+typedef struct __file_res {
+    res_pair *filePairs;
     char firstChar;
     char lastChar;
 } file_res;
 
 struct stat fileStat;
 
-void writeChar(char character);
-
-void writeNum(int charCount);
-
 void *worker(arg_t arg);
 
-void init_arg(arg_t arg, char *inputString);
+void init_arg(arg_t arg, char *inputString, bool *isFirstChar);
 
-void writePair(arg_t arg, char character, int charCount);
+void parse(char *src, bool *isFirstChar, int *charCount, char *lastChar, char *firstCharacter);
 
-void parseThreaded(char *src, char *buffer);
-
+void writePair(res_pair pair);
 
 #endif //WZIP_H
